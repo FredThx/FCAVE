a5d3fd1c2f11b2892530626ed3c16a0c55a62bd9
FredThx <fredthx@gmail.com>
2022-11-20 16:18:13 +0100

    Cave.options : dans init

--- components/cave.py
+++ components/cave.py
@@ -7,7 +7,15 @@ class Cave():
     '''
     def __init__(self, bdd):
         self.bdd = bdd
-    
+        self.options =  []
+        #TODO : mettre ça ailleurs (et donc renommer la classe en un truc de générique)
+        f_name = FieldText("name", table = 'vins', name = "Nom", placeholder = "Nom du vin")
+        f_color = FieldTextList("color", table = 'vins', name = "Couleur", placeholder = "Couleur du vin")
+        f_region = FieldTextForeign("region_name", table = "regions", link_table = 'appellations', name ="Région")
+        f_appellation = FieldTextForeign("appellation_name", table = "appellations", link_table = 'vins', name = "Appellation")
+        f_producer = FieldTextForeign("producer_name", table = "producers", link_table = 'vins', name = "Producteur")
+        self.add_options([f_name, f_color, f_region, f_appellation, f_producer])
+
     def get_vins(self, options = None):
         logging.debug(f"get_vins()...")
         vins = self.bdd.select('vins')
@@ -17,14 +25,6 @@ class Cave():
     def get_cards_vins(self, *args, **kwargs):
         return [CardVin(vin) for vin in self.get_vins(*args, **kwargs)]
     
-    options = [
-        FieldText("name", table = 'vins', name = "Nom", placeholder = "Nom du vin"),
-        FieldTextList("color", table = 'vins', name = "Couleur", placeholder = "Couleur du vin"),
-        FieldTextForeign("region_name", table = "regions", link_table = 'appellations', name ="Région"),
-        FieldTextForeign("appellation_name", table = "appellations", link_table = 'vins', name = "Appellation"),
-        FieldTextForeign("producer_name", table = "producers", link_table = 'vins', name = "Producteur"),
-    ]#TODO : sortir ça d'ici avec :
-
     def add_options(self, options:list[Field]):
         if type(options)!=list:
             options = [options]
