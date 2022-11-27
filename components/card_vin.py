bfdde30375ead423abbd6712e8ae1fb7ef19a696
FredThx <fredthx@gmail.com>
2022-11-27 13:07:06 +0100

    CardVin : colors

--- components/card_vin.py
+++ components/card_vin.py
@@ -4,11 +4,32 @@ from dash import html
 class CardVin(dbc.Card):
     '''A bootstrap card for vins
     '''
-
+    colors = {
+        'rouge' : 
+        {
+            'color' : '#47226A',
+            'inverse' : True,
+        },
+        'blanc' : 
+        {
+            'color' : '#EDEAA5',
+        },
+        'rosé' : 
+        {
+            'color' : '#E1A7D2',
+        },
+        None :
+        {
+            'color' : "grey"
+        }
+            }
     def __init__(self, vin):
         super().__init__(style={"width": "18rem"})
         self.vin = vin
         self.children = self.render()
+        self.className = "w-25 mx-1 my-1"
+        for attr_color, value in self.colors.get(vin.color).items():
+            self.__setattr__(attr_color, value)
     
     @property
     def data(self):
@@ -22,13 +43,15 @@ class CardVin(dbc.Card):
             #dbc.CardBody([html.P("todo")]),
             dbc.CardBody([
                 html.H6(self.vin.appellation, className = "card-title"),
-                html.P(f"Apogée entre {self.data['apogee_debut']} et {self.data['apogee_fin']}.", className = "card-text"),
-                html.P(f"Stock : {self.data['stock']}", className = "card-text"),
-                dbc.Row([
-                    dbc.Col(dbc.Button("Details", color="primary", id = "id"+"_bt_edit")),
-                    dbc.Col(dbc.Button("Supprime", color="primary", id = "id"+"_bt_remove")),
-                ]),
+                html.P(f"Apogée entre {self.data['apogee_debut'] or '___'} et {self.data['apogee_fin'] or '___'}.", className = "card-text"),
+                html.P(f"Stock : {self.data['stock'] or 0}", className = "card-text"),
             ]),
+            dbc.CardFooter(
+                dbc.Row([
+                    dbc.Col(dbc.Button("Details", color="primary", id = "id"+"_bt_edit"), width = 5),
+                    dbc.Col(dbc.Button("Supprime", color="primary", id = "id"+"_bt_remove"), width =5),
+                ]),
+            ),
             dbc.Tooltip("todo : tooltip header", placement = "top", target= id)
             ]
 
